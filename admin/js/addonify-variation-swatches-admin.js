361be45e7842277f4dbcdfa86760d68043502fd7
Sanil Shakya <sonill_ss@hotmail.com>
2020-10-12 10:50:24 +0545

    progress updates

--- admin/js/addonify-variation-swatches-admin.js
+++ admin/js/addonify-variation-swatches-admin.js
@@ -1,81 +1,32 @@
 (function( $ ) {
 	'use strict';
 
-	$(document).ready(function(){
-
-		// ios style switch
-		$('input.lc_switch').lc_switch();
-
-
-		var $require_login_btn = $('#addonify_wishlist_require_login');
-		var $redirect_to_login_btn = $('#addonify_wishlist_redirect_to_login');
-
-		// auto diable "redirect to login" if "require login" is disabled
-		$('body').delegate( '#addonify_wishlist_require_login', 'lcs-off', function() {
-			$redirect_to_login_btn.lcs_off();
-		});
-
-		$('body').delegate( '#addonify_wishlist_redirect_to_login', 'lcs-on', function() {
-
-			if( ! $require_login_btn.is(':checked') ){
-				setTimeout( function(){
-					$redirect_to_login_btn.lcs_off();
-				}, 400 );
-			}
-
-
-		});
-
-
-		// initiate wp color picker
-
-		if( $('.color-picker').length ){
-			$('.color-picker').wpColorPicker();
-		}
-
-
-		// code editor
-		if( $('#addonify_wishlist_custom_css').length ) {
-			var editorSettings = wp.codeEditor.defaultSettings ? _.clone( wp.codeEditor.defaultSettings ) : {};
-			editorSettings.codemirror = _.extend(
-				{},
-				editorSettings.codemirror,
-				{
-					indentUnit: 2,
-					tabSize: 2
-				}
-			);
-			var editor = wp.codeEditor.initialize( $('#addonify_wishlist_custom_css'), editorSettings );
-		}
-
-
-
-		// show hide content colors ------------------------------
-
-		let $style_options_sel = $('#addonify_wishlist_load_styles_from_plugin');
-		let $content_colors_sel = $('#addonify-content-colors-container');
-
-		// self activate
-		show_hide_content_colors();
-
-		// detect state change
-		$('body').delegate('#addonify_wishlist_load_styles_from_plugin', 'lcs-statuschange', function() {
-			show_hide_content_colors();
-		});
-
-		
-		function show_hide_content_colors(){
-
-			let state = $style_options_sel.is(":checked") 
-
-			if( state ){
-				$content_colors_sel.slideDown();
-			}
-			else{
-				$content_colors_sel.slideUp();
-			}
-		}
-	
-	})
+	/**
+	 * All of the code for your admin-facing JavaScript source
+	 * should reside in this file.
+	 *
+	 * Note: It has been assumed you will write jQuery code here, so the
+	 * $ function reference has been prepared for usage within the scope
+	 * of this function.
+	 *
+	 * This enables you to define handlers, for when the DOM is ready:
+	 *
+	 * $(function() {
+	 *
+	 * });
+	 *
+	 * When the window is loaded:
+	 *
+	 * $( window ).load(function() {
+	 *
+	 * });
+	 *
+	 * ...and/or other possibilities.
+	 *
+	 * Ideally, it is not considered best practise to attach more than a
+	 * single DOM-ready or window-load handler for a particular page.
+	 * Although scripts in the WordPress core, Plugins and Themes may be
+	 * practising this, we should strive to set a better example in our own work.
+	 */
 
 })( jQuery );
