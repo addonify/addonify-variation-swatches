909ff9d5274ced582cfca48216d0daea77f2a1ce
Sanil Shakya <sonill_ss@hotmail.com>
2020-11-02 15:40:30 +0545

    Update class-addonify-variation-swatches.php

--- includes/class-addonify-variation-swatches.php
+++ includes/class-addonify-variation-swatches.php
@@ -266,6 +266,7 @@ class Addonify_Variation_Swatches {
 
 
 		// Remove "Select options" button from (variable) products on the main WooCommerce shop page.
+		// show add to cart button in shop page.
 		add_filter( 'woocommerce_loop_add_to_cart_link', function( $button, $product ) {
 
 			if ( is_shop() && $product->is_type( 'variable' ) ) {
@@ -285,37 +286,37 @@ class Addonify_Variation_Swatches {
 		}, 10, 2 );
 
 		// ajax
-		add_action('wp_ajax_woocommerce_ajax_add_to_cart', 'woocommerce_ajax_add_to_cart');
-		add_action('wp_ajax_nopriv_woocommerce_ajax_add_to_cart', 'woocommerce_ajax_add_to_cart');
+		// add_action('wp_ajax_woocommerce_ajax_add_to_cart', 'woocommerce_ajax_add_to_cart');
+		// add_action('wp_ajax_nopriv_woocommerce_ajax_add_to_cart', 'woocommerce_ajax_add_to_cart');
 
-		function woocommerce_ajax_add_to_cart() {
+		// function woocommerce_ajax_add_to_cart() {
 
-            $product_id = apply_filters('woocommerce_add_to_cart_product_id', absint($_POST['product_id']));
-            $quantity = empty($_POST['quantity']) ? 1 : wc_stock_amount($_POST['quantity']);
-            $variation_id = absint($_POST['variation_id']);
-            $passed_validation = apply_filters('woocommerce_add_to_cart_validation', true, $product_id, $quantity);
-            $product_status = get_post_status($product_id);
+        //     $product_id = apply_filters('woocommerce_add_to_cart_product_id', absint($_POST['product_id']));
+        //     $quantity = empty($_POST['quantity']) ? 1 : wc_stock_amount($_POST['quantity']);
+        //     $variation_id = absint($_POST['variation_id']);
+        //     $passed_validation = apply_filters('woocommerce_add_to_cart_validation', true, $product_id, $quantity);
+        //     $product_status = get_post_status($product_id);
 
-            if ($passed_validation && WC()->cart->add_to_cart($product_id, $quantity, $variation_id) && 'publish' === $product_status) {
+        //     if ($passed_validation && WC()->cart->add_to_cart($product_id, $quantity, $variation_id) && 'publish' === $product_status) {
 
-                do_action('woocommerce_ajax_added_to_cart', $product_id);
+        //         do_action('woocommerce_ajax_added_to_cart', $product_id);
 
-                if ('yes' === get_option('woocommerce_cart_redirect_after_add')) {
-                    wc_add_to_cart_message(array($product_id => $quantity), true);
-                }
+        //         if ('yes' === get_option('woocommerce_cart_redirect_after_add')) {
+        //             wc_add_to_cart_message(array($product_id => $quantity), true);
+        //         }
 
-                WC_AJAX :: get_refreshed_fragments();
-            } else {
+        //         WC_AJAX :: get_refreshed_fragments();
+        //     } else {
 
-                $data = array(
-                    'error' => true,
-                    'product_url' => apply_filters('woocommerce_cart_redirect_after_error', get_permalink($product_id), $product_id));
+        //         $data = array(
+        //             'error' => true,
+        //             'product_url' => apply_filters('woocommerce_cart_redirect_after_error', get_permalink($product_id), $product_id));
 
-                echo wp_send_json($data);
-            }
+        //         echo wp_send_json($data);
+        //     }
 
-            wp_die();
-        }
+        //     wp_die();
+        // }
 
 	}
 
