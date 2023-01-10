cd31388a0cfe0d0902caf46b2836c18b72b3bec6
diwakar <50358405+diwakar2000@users.noreply.github.com>
2023-01-10 13:48:01 +0545

    fixed: empty selection if no image in image attribute

--- public/templates/attributes-options-image.php
+++ public/templates/attributes-options-image.php
@@ -24,7 +24,9 @@ defined( 'ABSPATH' ) || exit;
 <ul class="<?php echo esc_attr( $css_class ); ?>" >
 	<?php do_action( 'addonify_vs_start_of_variation_attributes_list', 'image' ); ?>
 	<?php foreach ( $options as $option_slug => $option_name ) : ?>
-		<li data-value="<?php echo esc_attr( $option_slug ); ?>" data-title="<?php echo esc_attr( $option_name[1] ); ?>" ><img class="adfy-vs adfy-image-vs" src="<?php echo esc_attr( $option_name[0] ); ?>" ></li>
+		<li data-value="<?php echo esc_attr( $option_slug ); ?>" data-title="<?php echo esc_attr( $option_name[1] ); ?>" >
+		<img class="adfy-vs adfy-image-vs" src="<?php echo esc_attr( $option_name[0] ); ?>" placeholder="<?php echo esc_attr( $option_name[1] ); ?>">
+	</li>
 	<?php endforeach; ?>
 	<?php do_action( 'addonify_vs_end_of_variation_attributes_list', 'image' ); ?>
 </ul>
